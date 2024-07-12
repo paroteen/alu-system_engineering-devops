59256c2c22a270390f2580c7ca86851031fb124d
Olivier ITANGISHAKA <codedreamer00@gmail.com>
2024-07-12 17:25:42 +0200

    ssh

---
+++ ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,4 @@
+# changes to configuration file
+exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
+        path    => '/bin/'
+}
---
+++ ssh/README.md
@@ -0,0 +1,5 @@
+0 - Use a private key 
+1 - Create an SSH key pair
+2 - Client configuration file
+3 - Changes to Authorized keys
+4 - Client Configuration file
